2d2329aefa799448b0c8e3f1125571785a2da899
Paolo Cardinale <alfio09@hotmail.com>
2023-11-29 19:38:43 +0100

    add temporary project crud with scaffolding

---
+++ MyPortfolioDotNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolioDotNet.Models;
+
+namespace MyPortfolioDotNet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Project { get; set; }
+    }
+}
--- MyPortfolioDotNet/Models/Project.cs
+++ MyPortfolioDotNet/Models/Project.cs
@@ -2,8 +2,12 @@
 {
     public class Project
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Technology { get; set; }
+        public string Screenshot { get; set; }
+
 
     }
 }
