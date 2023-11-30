0c36ff0bb0ebd515f33b9e8d47c8201536885964
Paolo Cardinale <alfio09@hotmail.com>
2023-11-30 16:50:33 +0100

    add Identity

--- MyPortfolioDotNet/Data/ApplicationDbContext.cs
+++ MyPortfolioDotNet/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyPortfolioDotNet.Models;
 
 namespace MyPortfolioDotNet.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
