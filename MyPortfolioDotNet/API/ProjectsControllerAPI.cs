625ef17dde60bdd6c5abdcd2703e1a97c8bf6469
alfio09 <alfio09@ALFIOZERONOVE>
2023-12-21 15:26:40 +0100

    add api

---
+++ MyPortfolioDotNet/API/ProjectsControllerAPI.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolioDotNet.Data;
+using MyPortfolioDotNet.Models;
+
+namespace MyPortfolioDotNet.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsControllerAPI : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsControllerAPI(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProjectsControllerAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProject()
+        {
+            return await _context.Project
+                  .Include(p => p.Images)
+                .Include(p => p.ProjectTechnologies)
+                .ThenInclude(pt => pt.Technology)
+                .OrderBy(p => p.OrderShow)
+                .ToListAsync();
+        }
+
+        // GET: api/ProjectsControllerAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(int id)
+        {
+            var project = await _context.Project.FindAsync(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return project;
+        }
+
+        // PUT: api/ProjectsControllerAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProject(int id, Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProjectsControllerAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Project>> PostProject(Project project)
+        {
+            _context.Project.Add(project);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProject", new { id = project.Id }, project);
+        }
+
+        // DELETE: api/ProjectsControllerAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            var project = await _context.Project.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Project.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return _context.Project.Any(e => e.Id == id);
+        }
+    }
+}
